8dd08dc993b5ac5aad5452690ade147ba4227b1b
calm-fork-7535 <komald.4@gmail.com>
2020-01-24 17:28:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 def game_hash
   {
     :home => {
-      :team_name => "Brooklyn Nets", 
-      :colors => ["Black", "White"] , 
-      :players => 
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"] ,
+      :players =>
         {"Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 },
         "Reggie Evans" => {:number => 30, :shoe =>14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 },
         "Brook Lopez" => {:number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15 },
@@ -13,9 +13,9 @@ def game_hash
     },
 
     :away => {
-      :team_name => "Charlotte Hornets", 
-      :colors => ["Turquoise", "Purple"], 
-      :players => 
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players =>
         {"Jeff Adrien" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
         "Bismak Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 7, :blocks => 15, :slam_dunks => 10 },
         "DeSagna Diop" => {:number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals =>4, :blocks => 5, :slam_dunks => 5 },
@@ -40,8 +40,8 @@ def player_with_longest_name
       end
     end
   end
-  player_with_longest_name      
-end  
+  player_with_longest_name
+end
 
 def long_name_steals_a_ton?#longest name the one with the most steals?
   most_steals = 0
@@ -55,7 +55,7 @@ def long_name_steals_a_ton?#longest name the one with the most steals?
     end
   end
   player_with_longest_name == stealer
-end 
+end
 
 
 
@@ -74,7 +74,7 @@ def winning_team
     end
   end
   team_name
-end        
+end
 
 
 
@@ -92,14 +92,14 @@ def most_points_scored
               if num > most_points
                 most_points = num
                 player_name = name
-              end 
+              end
             end
           end
         end
       end
     end
-  end   
-  return player_name          
+  end
+  return player_name
 end
 
 
@@ -124,9 +124,9 @@ def big_shoe_rebounds
       end
     end
   return game_hash[location][:players][bigfoot_player][:rebounds]
-  end      
+  end
 
-end  
+end
 
 
 def player_stats(player_name)
@@ -142,7 +142,7 @@ def player_stats(player_name)
       end
     end
   end
-end            
+end
 
 
 
@@ -158,8 +158,8 @@ def player_numbers(team_name)
       end
     end
   end
-  player_numbers          
-end  
+  player_numbers
+end
 
 def team_names
   #return array of team names
@@ -172,7 +172,7 @@ def team_names
     end
   end
   array
-end        
+end
 
 
 def team_colors(team_name)
@@ -183,8 +183,8 @@ def team_colors(team_name)
         return game_hash[location][:colors]
       end
     end
-  end      
-end  
+  end
+end
 
 def shoe_size(name)
   #returns shoe size for player
@@ -198,18 +198,18 @@ def shoe_size(name)
         end
       end
     end
-  end          
+  end
 end
 
 def num_points_scored(name)
-  #return num points for player 
+  #return num points for player
   game_hash.each do |location, info|
     info.each do |info, data|
       if info == :players
         data.each do |player_name, info|
           if name == player_name
             return game_hash[location][:players][name][:points]
-          end  
+          end
         end
       end
     end
